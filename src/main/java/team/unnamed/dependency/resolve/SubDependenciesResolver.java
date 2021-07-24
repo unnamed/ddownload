375370bd97a305bb73b663d7dcba1d32950701e1
Alex Avila <alex.ramon2004@gmail.com>
2021-07-24 00:19:58 -0500

    Added SubDependency Resolver

--- src/main/java/team/unnamed/dependency/resolve/SubDependenciesResolver.java
+++ src/main/java/team/unnamed/dependency/resolve/SubDependenciesResolver.java
@@ -1,9 +1,21 @@
 package team.unnamed.dependency.resolve;
 
+import org.apache.maven.model.Model;
+import org.apache.maven.model.Repository;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import team.unnamed.dependency.Dependency;
 import team.unnamed.dependency.MavenDependency;
+import team.unnamed.dependency.exception.ErrorDetails;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Resolves the dependencies of
@@ -13,7 +25,47 @@ public class SubDependenciesResolver implements DependencyResolver<Dependency> {
 
     @Override
     public List<Dependency> resolve(Dependency dependency) {
-        return null;
-    }
 
+        Set<String> repositories = new HashSet<>();
+
+        repositories.add(MavenDependency.CENTRAL);
+        ErrorDetails errorDetails = new ErrorDetails("Cannot resolve dependencies");
+        Set<Dependency> dependencies = new HashSet<>();
+        String pomUrl = dependency.getPossibleOriginUrls()[0];
+        try {
+            URL url = new URL(pomUrl);
+            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+            connection.setRequestProperty("User-Agent", "ddownloader");
+            try (BufferedInputStream inputStream = new BufferedInputStream(connection.getInputStream())) {
+                Model model = new MavenXpp3Reader().read(inputStream);
+                for (Repository repository : model.getRepositories()) {
+                    repositories.add(repository.getUrl());
+                }
+                for (org.apache.maven.model.Dependency mavenDependency : model.getDependencies()) {
+                    if (!mavenDependency.getScope().equalsIgnoreCase("compile")) continue;
+                    String version = mavenDependency.getVersion();
+                    Dependency subDependency = new MavenDependency(
+                            repositories.toArray(new String[]{}),
+                            mavenDependency.getGroupId(),
+                            mavenDependency.getArtifactId(),
+                            version,
+                            mavenDependency.getArtifactId().toLowerCase() + ".jar",
+                            false
+                    );
+                    dependencies.add(subDependency);
+
+                }
+            } catch (XmlPullParserException e) {
+                errorDetails.add(e);
+            }
+        } catch (IOException e) {
+            errorDetails.add(e);
+        }
+
+        if (errorDetails.errorCount() > 0) {
+            throw errorDetails.toResolveException();
+        }
+
+        return new ArrayList<>(dependencies);
+    }
 }
---
+++ src/test/java/team/unnamed/dependency/SubDependencyResolverTest.java
@@ -0,0 +1,29 @@
+package team.unnamed.dependency;
+
+import org.junit.jupiter.api.Test;
+import team.unnamed.dependency.annotation.MavenLibrary;
+import team.unnamed.dependency.logging.LogStrategy;
+import team.unnamed.dependency.resolve.DependencyResolver;
+import team.unnamed.dependency.resolve.MavenDependencyResolver;
+import team.unnamed.dependency.resolve.SubDependenciesResolver;
+
+import java.util.List;
+
+@MavenLibrary(
+        groupId = "com.github.twitch4j",
+        artifactId ="twitch4j",
+        filename = "t3.jar"
+)
+public class SubDependencyResolverTest {
+
+    @Test
+    public void test() {
+
+        DependencyResolver<Class<?>> resolver = new MavenDependencyResolver(LogStrategy.getSilent());
+        List<Dependency> dependencies = resolver.resolve(SubDependencyResolverTest.class);
+        for (Dependency dependency : dependencies ) {
+            List<Dependency> subDependency = new SubDependenciesResolver().resolve(dependency);
+        }
+    }
+
+}
